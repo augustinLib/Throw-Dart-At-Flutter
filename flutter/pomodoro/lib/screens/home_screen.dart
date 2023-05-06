7e9be14fb6110b6fa426f04fb9fb29046a0328eb
augustinLib <2bambitious@gmail.com>
2023-05-06 22:02:23 +0900

    Feat: Add studied contents

--- flutter/pomodoro/lib/screens/home_screen.dart
+++ flutter/pomodoro/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static const twentyFiveMinutes = 1500;
+  int totalSeconds = twentyFiveMinutes;
+  bool isRunning = false;
+  int totalPomodoros = 0;
+  late Timer timer;
+
+  void onTick(Timer timer) {
+    if (totalSeconds == 0) {
+      timer.cancel();
+      setState(() {
+        totalSeconds = twentyFiveMinutes;
+        totalPomodoros++;
+        isRunning = !isRunning;
+      });
+    } else {
+      setState(() {
+        totalSeconds--;
+      });
+    }
+  }
+
+  void onStartPress() {
+    // Timer.periodic(duration, callback)
+    // duration마다 callback을 실행하는 Timer를 생성한다.
+    timer = Timer.periodic(
+      const Duration(seconds: 1),
+      onTick,
+    );
+    setState(() {
+      isRunning = !isRunning;
+    });
+  }
+
+  String format(int seconds) {
+    var duration = Duration(seconds: seconds);
+    // substring(2, 7) : 문자열에서 2번째부터 7번째까지의 문자열을 반환한다.
+    return duration.toString().split('.').first.substring(2, 7);
+  }
+
+  void onPausePress() {
+    // timer.cancel()을 호출하면 timer는 종료된다.
+    timer.cancel();
+    setState(() {
+      isRunning = !isRunning;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Container(
               alignment: Alignment.bottomCenter,
               child: Text(
-                "25:00",
+                format(totalSeconds),
                 style: TextStyle(
                   color: Theme.of(context).cardColor,
                   fontSize: 89,
@@ -34,8 +82,10 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: () {},
-                icon: const Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePress : onStartPress,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_outline
+                    : Icons.play_circle_outline),
               ),
             ),
           ),
@@ -47,6 +97,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   child: Container(
                     decoration: BoxDecoration(
                       color: Theme.of(context).cardColor,
+                      borderRadius: BorderRadius.circular(50),
                     ),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -61,7 +112,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                         ),
                         Text(
-                          "0",
+                          "$totalPomodoros",
                           style: TextStyle(
                             fontSize: 58,
                             fontWeight: FontWeight.w600,
